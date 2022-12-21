c501ffad95d850b8e06862dd40f52bdda4c582df
Pascal P <lacsap_666@yahoo.fr>
2022-12-22 11:37:22 +1300

    grade-school review

--- grade-school/grade-school.ts
+++ grade-school/grade-school.ts
@@ -1,35 +1,29 @@
+type Pair = [string, string[]]
+
 class Student {
-  readonly name: string;
-  grade: number;
+  readonly _name: string;
+  _grade: number;
 
   constructor(name: string, grade: number) {
     if (grade <= 0) {
       throw new Error(`invalid grade: ${grade} - must be >0`)
     }
-    this.name = name;
-    this.grade = grade;
+    this._name = name;
+    this._grade = grade;
   }
 }
 
 class GradeSchool {
-  students: Student[];
-  roster: Map<string, string[]>;
+  _students: Student[];
+  _roster: Map<string, string[]>;
 
   constructor() {
-    this.students = [];
-    this.roster = new Map<string, string[]>();
+    this._students = [];
+    this._roster = new Map<string, string[]>();
   }
 
   studentRoster(): Map<string, string[]> {
-    // make a deepcopy
-    let lst: any = [];
-    /*
-    this.roster.forEach((val, key) => {
-      const obj = Object.assign({}, { key: key, val: val })
-      lst.push([obj.key, [...obj.val]]);
-    });
-    */
-    this.roster.forEach((val, key) => lst.push([key, [...val]]));
+    const lst = this.deepClone()
     return new Map<string, string[]>(lst);
   }
 
@@ -38,41 +32,49 @@ class GradeSchool {
       throw new Error(`invalid grade: ${grade} - must be >0`)
     }
 
-    let ix = this.students.findIndex(s => s.name === name);
-    if (ix > - 1) {
+    const ix = this._students.findIndex(s => s._name === name);
+    if (ix > -1) {
       // update
       this.updateRoster(name, grade, ix);
-      this.students[ix].grade = grade;
+      this._students[ix]._grade = grade;
+      return;
     }
-    else {
-      // create
-      const student = new Student(name, grade);
-      this.students.push(student);
-      ix = this.students.length - 1;
-      this.updateRoster(name, grade, ix);
-    }
+    // create
+    this._students = [
+      ...this._students,
+      new Student(name, grade)
+    ]
+    this.updateRoster(name, grade, this._students.length - 1);
   }
 
   studentsInGrade(grade: number): string[] {
     if (grade <= 0) return [];
-    const str_gr = String(grade);
+    const strGr = String(grade);
     // copy
-    const lst = this.roster.get(str_gr) || [];
-    return [...lst];
+    return [
+      ...(this._roster.get(strGr) ?? [])
+    ];
   }
 
   private updateRoster(name: string, grade: number, ix: number): void {
-    const stu_gr = String(this.students[ix].grade);
+    const stuGr = String(this._students[ix]._grade);
+    const lst = this._roster.get(stuGr) ?? [];
+    this._roster.set(stuGr, lst.filter((n: string) => n !== name));
 
-    let lst = this.roster.get(stu_gr) || [];
-    const nlst = lst.filter((n: string) => n !== name);
-    this.roster.set(stu_gr, nlst);
+    const stuNgr = String(grade);
+    const nlst = [
+      ...lst,
+      name
+    ].sort();
+    this._roster.set(stuNgr, nlst);
+  }
 
-    const stu_ngr = String(grade);
-    lst = this.roster.get(stu_ngr) || [];
-    lst.push(name)
-    lst.sort();
-    this.roster.set(stu_ngr, lst);
+  private deepClone() {
+    return [...this._roster]
+      .reduce((al: Pair[], [key, val]) => {
+        al.push([key, [...val]]);
+        return al
+      }, []);
   }
 }
 
