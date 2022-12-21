e1adb44c13e7079805be9631e134d3bb4d121182
Pascal P <lacsap_666@yahoo.fr>
2022-12-22 12:02:47 +1300

    rational-numbers review

--- rational-numbers/rational-numbers.ts
+++ rational-numbers/rational-numbers.ts
@@ -8,55 +8,50 @@ interface IRational {
   expreal(n: number): number
 }
 
+type TRational = [num: number, den: number]
+
 class Rational implements IRational {
-  private _num: number;
-  private _den: number;
+  private _pair: TRational;
 
   constructor(num: number, den: number = 1) {
     if (den === 0) { throw new Error(`Denominator cannot be 0 (num: ${num}, den: ${den}`) }
-
     if (num === den) {
-      this._num = this._den = 1;
+      this._pair = [1, 1];
+      return;
     }
-    else {
-      let sign = (num >= 0 && den >= 0) || (num < 0 && den < 0) ? 1 : -1;
-      [num, den] = [Math.abs(num), Math.abs(den)];
-      let [n, d] = Rational.divByGcd(num, den);
-      this._num = sign * n;
-      this._den = d;
-    }
+    this._pair = this.canonicalize(num, den);
   }
 
   get num(): number {
-    return this._num;
+    return this._pair[0];
   }
 
   get den(): number {
-    return this._den;
+    return this._pair[1];
   }
 
   abs(): Rational {
-    return new Rational(Math.abs(this._num), Math.abs(this._den));
+    return new Rational(Math.abs(this._pair[0]), Math.abs(this._pair[1]));
   }
 
   @checkArg
   add(r: Rational): Rational {
-    return new Rational(this._num * r._den + r._num * this._den, this._den * r._den);
+    return new Rational(this._pair[0] * r._pair[1] + r._pair[0] * this._pair[1], this._pair[1] * r._pair[1]);
   }
 
   @checkArg
   sub(r: Rational): Rational {
-    return new Rational(this._num * r._den - r._num * this._den, this._den * r._den);
+    return new Rational(this._pair[0] * r._pair[1] - r._pair[0] * this._pair[1], this._pair[1] * r._pair[1]);
   }
 
   @checkArg
   mul(r: Rational): Rational {
-    return new Rational(this._num * r._num, this._den * r._den);
+    return new Rational(this._pair[0] * r._pair[0], this._pair[1] * r._pair[1]);
   }
 
   @checkArg
   div(r: Rational): Rational {
-    return new Rational(this._num * r._den, this._den * r._num);
+    return new Rational(this._pair[0] * r._pair[1], this._pair[1] * r._pair[0]);
   }
 
   exprational(n: number): Rational {
@@ -65,19 +60,18 @@ class Rational implements IRational {
       Math.floor(n) !== n) { throw new Error("NaN or Not an Integer"); }
 
     if (n >= 0) {
-      return new Rational(this._num ** n, this._den ** n);
+      return new Rational(this._pair[0] ** n, this._pair[1] ** n);
     }
-    else {
-      n = Math.abs(n);
-      return new Rational(this._den ** n, this._num ** n);
-    }
+
+    n = Math.abs(n);
+    return new Rational(this._pair[1] ** n, this._pair[0] ** n);
   }
 
   expreal(n: number, eps = 1e-12): number {
     if (n === undefined ||
       (typeof n) !== 'number') { throw new Error("NaN"); }
 
-    const res = n ** (this._num / this._den);
+    const res = n ** (this._pair[0] / this._pair[1]);
     if (Math.abs(Math.ceil(Math.abs(res)) - Math.abs(res)) <= eps) {
       return Math.ceil(res);
     }
@@ -97,17 +91,21 @@ class Rational implements IRational {
   }
 
   private static gcd(num: number, den: number): number {
-    [num, den] = num < den ? [den, num] : [num, den];
-
-    if (den === 0) { return num; }
-
-    let r = num;
+    let [num_, den_] = num < den ? [den, num] : [num, den];
+    if (den_ === 0) { return num_; }
+    let r = num_;
     while (r > 1) {
-      r = num % den;
-      [num, den] = [den, r];
+      r = num_ % den_;
+      [num_, den_] = [den_, r];
     }
+    return r === 0 ? num_ : r;
+  }
 
-    return r === 0 ? num : r;
+  private canonicalize(num: number, den: number): TRational {
+    const sign = (num >= 0 && den >= 0) || (num < 0 && den < 0) ? 1 : -1;
+    [num, den] = [Math.abs(num), Math.abs(den)];
+    const [n, d] = Rational.divByGcd(num, den);
+    return [sign * n, d];
   }
 }
 
@@ -135,21 +133,20 @@ function checkArg(_target: any, _propertyKey: string, descriptor: PropertyDescri
   const original = descriptor.value;
 
   descriptor.value = function(...args: any) {
-    // console.log('params: ', ...args);
     const r = args[0];
 
-    if (r === undefined) { throw new Error("NaN"); }
+    if (!r) { throw new Error("NaN"); }
 
     if (r instanceof Rational) {
       return original.call(this, ...args);
     }
-    else if ((typeof r) === 'number' && Math.floor(r) === r) {
+
+    if ((typeof r) === 'number' && Math.floor(r) === r) {
       // we want an integer, not a real
       return original.call(this, new Rational(r, 1));
     }
-    else {
-      throw new Error("NaN");
-    }
+
+    throw new Error("NaN");
   }
 }
 
