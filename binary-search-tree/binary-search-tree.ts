78d3c3bf5a56e27cc6d912bb213b428b205e409d
Pascal P <lacsap_666@yahoo.fr>
2022-12-22 14:32:54 +1300

    binary-search-tree review

--- binary-search-tree/binary-search-tree.ts
+++ binary-search-tree/binary-search-tree.ts
@@ -1,68 +1,69 @@
-class TreeNode {
-  data: any;
-  left: TreeNode | undefined;
-  right: TreeNode | undefined;
+type TTreeNode<T> = TreeNode<T> | undefined;
 
-  constructor(data: any, left?: TreeNode | undefined, right?: TreeNode | undefined) {
+class TreeNode<T> {
+  data: T;
+  left: TTreeNode<T>;
+  right: TTreeNode<T>;
+
+  constructor(data: T, left?: TreeNode<T> | undefined, right?: TreeNode<T> | undefined) {
     this.data = data;
     this.left = left;
     this.right = right;
   }
 }
 
-class BinarySearchTree {
-  _root: TreeNode | undefined;
+class BinarySearchTree<T> {
+  _root: TTreeNode<T>;
 
-  constructor(data: any) {
-    if (typeof data === 'undefined') {
+  constructor(data: T) {
+    if (this.isUndefinedOrNull(data))  {
       this._root = undefined;
-      return
+      return;
     }
     this._root = new TreeNode(data);
   }
 
-  get data(): any {
-    if (typeof this._root === 'undefined') {
+  get data(): T {
+    if (this.isUndefinedOrNull(this._root)) {
       throw new Error("Empty Tree");
     }
-    return this._root.data;
+    return this._root!.data;
   }
 
-  get right(): TreeNode | undefined {
+  get right(): TTreeNode<T> {
     if (typeof this._root === 'undefined') {
       throw new Error("Empty Tree");
     }
     return this._root.right;
   }
 
-  get left(): TreeNode | undefined {
+  get left(): TTreeNode<T> {
     if (typeof this._root === 'undefined') {
       throw new Error("Empty Tree");
     }
     return this._root.left;
   }
 
-  insert(data: any): TreeNode | undefined {
-    if (typeof data === 'undefined') {
+  insert(data: T): TTreeNode<T> {
+    if (this.isUndefinedOrNull(data)) {
       throw new Error("nothing to insert in current tree...");
     }
 
     const node = new TreeNode(data);
-    if (typeof this._root === 'undefined') {
+    if (this.isUndefinedOrNull(this._root)) {
       return this._root = node;
     }
-    return this._insert(this._root, this._root, node, 'left');
+
+    return this._insert(this._root as TreeNode<T>, this._root as TreeNode<T>, node, 'left');
   }
 
-  each(fn: any): TreeNode | undefined {
+  each(fn: (_a: T) => any): TTreeNode<T> {
     return this._dfs_inf(this._root, fn);
   }
 
   // helpers
-
-  private _insert(parent: TreeNode, root: TreeNode | undefined, node: TreeNode,
-    dir = 'left'): TreeNode | undefined {
-    if (typeof root === 'undefined') {
+  private _insert(parent: TreeNode<T>, root: TTreeNode<T>, node: TreeNode<T>, dir = 'left'): TTreeNode<T> {
+    if (this.isUndefinedOrNull(root)) {
       if (dir === 'left') {
         parent.left = node;
       }
@@ -72,24 +73,27 @@ class BinarySearchTree {
       return;
     }
 
-    if (node.data < root.data) {
-      return this._insert(root, root.left, node, 'left');
+    if (node.data < root!.data) {
+      return this._insert(root!, root!.left, node, 'left');
     }
-    else if (node.data > root.data) {
-      return this._insert(root, root.right, node, 'right');
+
+    if (node.data > root!.data) {
+      return this._insert(root!, root!.right, node, 'right');
     }
-    else {
-      this._insert(root, root.left, node, 'left');
-    }
+
+    this._insert(root!, root!.left, node, 'left');
   }
 
-  private _dfs_inf(root: TreeNode | undefined, fn: any): TreeNode | undefined {
+  private _dfs_inf(root: TTreeNode<T>, fn: (_a: T) => any): TTreeNode<T> {
     if (typeof root === 'undefined') { return undefined; }
     this._dfs_inf(root.left, fn);
     fn(root.data);
     return this._dfs_inf(root.right, fn);
   }
 
+  private isUndefinedOrNull(data: T | TTreeNode<T> | null): boolean {
+    return typeof data === 'undefined' || data === null;
+  }
 }
 
 export default BinarySearchTree;
