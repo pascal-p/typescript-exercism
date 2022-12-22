3c61a0490fde30706cd61f66e1d44ed6ad548649
Pascal P <lacsap_666@yahoo.fr>
2022-12-22 13:44:21 +1300

    simple-cipher review

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -6,81 +6,79 @@ const LEN_ALPHA: number = ALPHA.length;
 const getRandomInt = (min: number = 0, max: number = LEN_ALPHA) => Math.floor(Math.random() * (max - min) + min);
 
 const check = (input: string): void => {
-    if (!input.match(/^[a-z]+$/)) { throw new Error('Expecting only Latin lower case characters'); }
+  if (!input.match(/^[a-z]+$/)) { throw new Error('Expecting only Latin lower case characters'); }
 }
 
+type Pair = [number, number];
 
 class SimpleCipher {
-    // { a → 0, b → 1, c → 2, d → 3, e → 4, ... }
-    private static HSH: Map<string, number> = ALPHA.split('').reduce(
-        (hsh, ch, ix) => hsh.set(ch, ix),
-        new Map()
-    );
+  // { a → 0, b → 1, c → 2, d → 3, e → 4, ... }
+  private static HSH: Map<string, number> =
+    ALPHA.split('').reduce((hsh, ch, ix) => hsh.set(ch, ix), new Map());
 
-    // { 0 → "a", 1 → "b", 2 → "c", 3 → "d", 4 → "e", ... }
-    private static REV_HSH: Map<number, string> = [...SimpleCipher.HSH.entries()].reduce(
-        (hsh, [k, v]) => hsh.set(v, k),
-        new Map());
+  // { 0 → "a", 1 → "b", 2 → "c", 3 → "d", 4 → "e", ... }
+  private static REV_HSH: Map<number, string> =
+    [...SimpleCipher.HSH.entries()].reduce((hsh, [k, v]) => hsh.set(v, k), new Map());
 
-    readonly key: string;
-    readonly keyLen: number;
+  readonly key: string;
+  readonly keyLen: number;
 
-    constructor(key?: Optional<string>) {
-        if (typeof key === 'undefined') {
-            key = '';
-            for (let ix = 0; ix < 100; ix++) {
-                key += SimpleCipher.REV_HSH.get(getRandomInt());
-            }
-        }
-        else {
-            check(key);
-        }
-        this.key = key.toLowerCase();
-        this.keyLen = this.key.length;
+  constructor(key?: Optional<string>) {
+    if (typeof key === 'undefined') {
+      key = '';
+      for (let ix = 0; ix < 100; ix++) {
+        key += SimpleCipher.REV_HSH.get(getRandomInt());
+      }
     }
-
-    encode(plain: string, canThrow: boolean = true): string {
-        if (canThrow) { check(plain); }
-        return this.transcode(plain, this.transFn);
+    else {
+      check(key);
     }
+    this.key = key.toLowerCase();
+    this.keyLen = this.key.length;
+  }
 
-    decode(ciphered: string, canThrow: boolean = true): string {
-        if (canThrow) { check(ciphered); }
-        return this.transcode(ciphered, this.revTransFn);
-    }
+  encode(plain: string, canThrow: boolean = true): string {
+    if (canThrow) { check(plain); }
+    return this.transcode(plain, this.transFn.bind(this));
+  }
 
-    //
-    // internal helpers
-    //
-    private transcode(input: string, fn: any): string {
-        // input can contains non latin chars that we ignore...
-        return input.toLowerCase().split("")
-            .filter((ch: string) => ch.match(/[a-z]/))
-            .map((ch: string, ix: number) => SimpleCipher.REV_HSH.get(fn(this, ch, ix)))
-            .join('');
-    }
+  decode(ciphered: string, canThrow: boolean = true): string {
+    if (canThrow) { check(ciphered); }
+    return this.transcode(ciphered, this.revTransFn.bind(this));
+  }
 
-    private getIndex(ch: string, ix: number) {
-        const och = this.key.charAt(ix % this.keyLen);
-        const kx = SimpleCipher.HSH.has(ch) ? SimpleCipher.HSH.get(ch) : -1;
-        const jx = SimpleCipher.HSH.has(och) ? SimpleCipher.HSH.get(och) : -1;
-        if (kx == -1 || jx == -1) {
-            throw new Error('key undefined'); // ch or och
-        }
-        return [kx, jx];
-    }
+  //
+  // internal helpers
+  //
+  private transcode(input: string, fn: (_s: string, _n: number) => number): string {
+    // input can contains non latin chars that we ignore...
+    return input.toLowerCase().split("")
+      .filter((ch: string) => ch.match(/[a-z]/))
+      .map((ch: string, ix: number) => SimpleCipher.REV_HSH.get(fn(ch, ix)))
+      .join('');
+  }
 
-    private revTransFn(self: any, ch: string, ix: number) {
-        let [kx, jx] = self.getIndex(ch, ix);
-        jx = kx - jx;
-        jx = jx < 0 ? LEN_ALPHA + jx : jx;
-        return jx % LEN_ALPHA;
+  private getIndex(ch: string, ix: number): Pair {
+    const och = this.key.charAt(ix % this.keyLen);
+    const kx = (SimpleCipher.HSH.has(ch) ? SimpleCipher.HSH.get(ch) : -1) as number;
+    const jx = (SimpleCipher.HSH.has(och) ? SimpleCipher.HSH.get(och) : -1) as number;
+    if (kx == -1 || jx == -1) {
+      throw new Error('key undefined'); // ch or och
     }
+    return [kx, jx];
+  }
 
-    private transFn(self: any, ch: string, ix: number) {
-        const [kx, jx] = self.getIndex(ch, ix);
-        return (jx + kx) % LEN_ALPHA;
-    }
+  private revTransFn(ch: string, ix: number): number {
+    let [kx, jx] = this.getIndex(ch, ix);
+    jx = kx - jx;
+    jx = jx < 0 ? LEN_ALPHA + jx : jx;
+    return jx % LEN_ALPHA;
+  }
+
+  private transFn(ch: string, ix: number): number {
+    const [kx, jx] = this.getIndex(ch, ix);
+    return (jx + kx) % LEN_ALPHA;
+  }
 }
 
 export default SimpleCipher
